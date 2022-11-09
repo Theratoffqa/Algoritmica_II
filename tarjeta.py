02146a50ae9accaa3897482840fccdaf74554152
Jose Quispe <jose.quispe35@unmsm.edu.pe>
2022-11-09 12:05:31 -0500

    Arreglando metodo verficarCaducidad y reordenando

--- tarjeta.py
+++ tarjeta.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import json
 
 class Tarjeta:
@@ -14,20 +14,22 @@ class Tarjeta:
         passed = False
         if len(str(N_tarjeta)) == 16:
             passed = True
-            return passed
+        return passed
     
-    def verificarCaducidad(F_Venci,F_Actu):
+    def verificarCaducidad(fechaVenci):
         passed = False
-        if F_Venci > F_Actu:
+        fechaActual = datetime.datetime.now()
+        fechaV = datetime.datetime.strptime(fechaVenci, "%d/%m/%Y")
+        if fechaV > fechaActual:
             passed = True
-            return passed
+        return passed
     
-    def verificarBloqueo(Num_Tarjeta):
+    def verificarBloqueo(num_Tarjeta):
         passed = False
         with open("numerobloqueado.json", "r") as f:
             numerosBloqueados = json.load(f)
         for element in numerosBloqueados:
-            if element["numero"] == Num_Tarjeta:
+            if element["numero"] == num_Tarjeta:
                 passed = True
         return passed        
             
@@ -39,25 +41,21 @@ class Tarjeta:
 if __name__ == "__main__":
     menu="""Ingresar tarjeta"""
 
-    NombreTarjeta = input("Nombre: ")
-    ApellidoTarjeta = input("Apellido: ")
-    NumeroTarjeta = int(input("Tarjeta: "))
-    CodigoTarjeta = int(input("Codigo de seguridad: "))
-    FechaCaducidadTarjeta = input("Fecha Caducidad: ")
-    # if Tarjeta.verificarTarjeta(NumeroTarjeta):
-    #     print("Numero de Tarjeta valido")
-    #     valido = True
-    # else:
-    #     print("Numero de Tarjeta invalido")
+    nombreTarjeta = input("Nombre: ")
+    apellidoTarjeta = input("Apellido: ")
+    numeroTarjeta = int(input("Tarjeta: "))
+    codigoTarjeta = int(input("Codigo de seguridad: "))
+    fechaCaducidadTarjeta = input("Fecha Caducidad: ")
 
-    # if valido == True:
-    #     if Tarjeta.verificarCaducidad(FechaCaducidadTarjeta,datetime.date()):
-    #         print("Tarjeta vigente")
-    #     else:
-    #         print("Tarjeta vencida")
-
-    if Tarjeta.verificarBloqueo(NumeroTarjeta):
-        print("Tarjeta bloqueada")
+    if Tarjeta.verificarTarjeta(numeroTarjeta):
+        print("Numero de Tarjeta valido")
+        if Tarjeta.verificarBloqueo(numeroTarjeta) == False:
+            print("Tarjeta operativa")
+            if Tarjeta.verificarCaducidad(fechaCaducidadTarjeta):
+                print("Tarjeta vigente")
+            else:
+                print("Tarjeta vencida")
+        else:
+            print("Tarjeta bloqueada")
     else:
-        print("Tarjeta operativa")
-
+        print("Numero de Tarjeta invalido")
