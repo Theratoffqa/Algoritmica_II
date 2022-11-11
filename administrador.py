349313d7fb2b3ea3e9b613fd02c4ec5436c4538f
GHOPsT <mggsalvator@gmail.com>
2022-11-10 21:54:54 -0500

    nuevos metodos agregados a cliente

--- administrador.py
+++ administrador.py
@@ -1,12 +1,20 @@
 from user import Usuario
+import json
 
 class Administrador(Usuario):
 
     def __init__(self,usuario,contrasenia,nombre,apellido,correo, llaveMaestra):
         super().__init__(usuario,contrasenia,nombre,apellido,correo)
-        self._llaveMaestra = llaveMaestra
+        self._contrasenia = contrasenia
        
-        
-
-    def actualizarCatalogo(self):
-        pass
+    def actualizarDatos(user,contr):
+        with open("usuarios.json", "r") as f:
+            usuario = json.load(f)
+        if element["usuario"] == user and element["contrasenia"] == contr: 
+            Contra_actual = input("Contraseña actual : ")
+            if Contra_actual == contr:
+                Nuevo_correo = input("Nuevo correo: ")
+                for element in usuario:
+                    element["correo"] = Nuevo_correo
+            else: 
+                print("Contraseña no coincide")
